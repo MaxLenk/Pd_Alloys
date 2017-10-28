26952e5e9b5bca44979dbcf8817af75822ab6b61
Max Lenk <mlenk3@joe-6.pace.gatech.edu>
2017-10-28 17:43:18 -0400

    First commit

---
+++ Pd/Pd_geometry_optimization.py
@@ -0,0 +1,34 @@
+from ase.io import read
+from espresso import espresso
+from ase.optimize import QuasiNewton
+
+#f = open('WCl_adsorption/W_geometry_optimization_500_441/energy.txt', 'w')
+#f.write('starting')  # python will convert \n to os.linesep
+
+Pd_slab_path = 'Pd_Alloys/Pd/Pd.traj'
+output_directory = 'Pd_Alloys/Pd/Pd_geometry_optimization_600_661/'
+
+Pd_slab=read(Pd_slab_path)
+Pd_slab.calc=espresso(pw=600,
+                       dw=4500,
+                       kpts=(6,6,1),
+                       xc='PBE',
+                       outdir= output_directory + 'E_Pd_slab',#espresso outdirectory saved
+                                            #here                                    
+                       convergence={'energy':1e-6,
+                                    'mixing':0.05,
+                                    'mixing_mode':'local-TF',
+                                    'maxsteps':1000,
+                                    'diag':'cg'})
+
+relax_Pd_slab=QuasiNewton(Pd_slab,
+                           logfile=output_directory + 'opt.log',
+                           trajectory=output_directory + 'opt.traj',
+                           restart=output_directory + 'opt.pckl') #ase output
+relax_Pd_slab.run(fmax=0.05)
+
+E_Pd_slab=Pd_slab.get_potential_energy()
+#f.write(E_W_slab)
+#f.write('\n END')
+#f.close()  # you can omit in most cases as the destructor will call it
+print(E_Pd_slab)
